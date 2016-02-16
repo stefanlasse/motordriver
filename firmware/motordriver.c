7e94c8cf9989716b1efb181b5929e228acb99b57
Stefan Lasse <lasse.stefan@gmx.de>
2016-02-16 10:03:00 +0100

    init PI3 branch

--- firmware/motordriver.c
+++ firmware/motordriver.c
@@ -8,7 +8,7 @@
 
 /*
  *   Motordriver: 3. PI, Uni Stuttgart
- *   Copyright (C) 2014  Stefan Lasse
+ *   Copyright (C) 2014, 2015, 2016  Stefan Lasse
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
